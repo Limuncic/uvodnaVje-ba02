df56300692eecf5dcd75eb2d444ed612559ce38d
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-09-20 09:02:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVježba02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c, d;
+
+            a = 10;
+            b = 7;
+            c = 12.5;
+            d = 16.7;
+
+            Console.WriteLine("Prosiječna vrijednost je: " + ((a + b + c + d) / 4));
+
+            Console.ReadKey();
+        }
+    }
+}
